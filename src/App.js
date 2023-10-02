11d7cff6b710a5e7e02bd21b9eacea769bfff97e
amiosamu <damitullaev@gmail.com>
2023-10-02 18:18:07 +0600

    Performing Dijkstra's algorithm

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DSAVisualizer from './DSAVisualizer/DSAVisualizer';
+
+function App() {
+    return (
+        <div className="App">
+            <DSAVisualizer></DSAVisualizer>
+        </div>
+    );
+}
+
+export default App;
